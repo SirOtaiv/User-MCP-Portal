881faf5e9baa07a99aaa63ad0f81c69af576b1ff
SirOtaiv <rauotavio7@gmail.com>
2025-10-30 19:47:47 -0300

    Add ag-grid dependencies and register AllCommunityModule in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  ModuleRegistry.registerModules([AllCommunityModule]);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
